39c4e1bdf693db41d108802ea596e69f96c38280
ryanturner <ryan.e.t@gmail.com>
2014-03-02 21:26:16 -0600

    Download button works; progress bar and status bar work; minor cleanup

--- node_tool/common/helpers.py
+++ node_tool/common/helpers.py
@@ -3,48 +3,43 @@ Created on Feb 6, 2014
 
 @author: ryan_turner
 '''
-import urllib.request
-import shutil
+
 import os
 import zipfile
+import urllib
 
-class firmware(object):
-    
-    def download(self):
-        directory = "firmware"
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        # Download the mikrotik routeros firmware
-        file_name = "all_packages-mipsbe-6.9.zip"
-        url = "http://download2.mikrotik.com/routeros/6.9/"
-        try:
-            with urllib.request.urlopen(url + file_name) as response, open(directory + "/" + file_name, 'wb') as out_file:
-                shutil.copyfileobj(response, out_file)
-        except Exception as e:
-            print("Error downloading firmware.")
-        try:
-            with zipfile.ZipFile(directory + "/" + file_name) as zipped_firmware:
-                zipped_firmware.extractall(directory)
-        except Exception as e:
-            print("Error unzipping firmware.")
-        try:
-            os.remove(directory + "/" + file_name)
-        except Exception as e:
-            print("Error deleting firmware zip file.")
-            
-        # Get the branding package too
-        file_name = "HamWAN-2.9.48.dpk"
-        url = "https://github.com/HamWAN/hamwan-branding/raw/master/Mikrotik%20Rebranding/"
-        try:
-            with urllib.request.urlopen(url + file_name) as response, open(directory + "/" + file_name, 'wb') as out_file:
-                shutil.copyfileobj(response, out_file)
-        except Exception as e:
-            print("Error downloading branding package.")
+def firmware_download():
+    directory = "firmware"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    # Download the mikrotik routeros firmware
+    file_name = "all_packages-mipsbe-6.9.zip"
+    url = "http://download2.mikrotik.com/routeros/6.9/"
+    try:
+        urllib.urlretrieve(url + file_name, directory + "/" + file_name)
+    except:
+        print("Error downloading firmware.")
+        return False
+    try:
+        with zipfile.ZipFile(directory + "/" + file_name) as zipped_firmware:
+            zipped_firmware.extractall(directory)
+    except:
+        print("Error unzipping firmware.")
+        return False
+    try:
+        os.remove(directory + "/" + file_name)
+    except:
+        print("Error deleting firmware zip file.")
+        return False
         
-        return
-    
-    def __init__(self):
-        '''
-        Constructor
-        '''
+    # Get the branding package too
+    file_name = "HamWAN-2.9.48.dpk"
+    url = "https://github.com/HamWAN/hamwan-branding/raw/master/Mikrotik%20Rebranding/"
+    try:
+        urllib.urlretrieve(url + file_name, directory + "/" + file_name)
+    except:
+        print("Error downloading branding package.")
+        return False
+    return True
+        
 
--- node_tool/gui/controller/config.py
+++ node_tool/gui/controller/config.py
@@ -1,19 +1,26 @@
 from . import helper;
-from PySide import QtGui
+from PySide import QtGui, QtCore
 from ...models import Configuration
+from ...common import helpers
+import time
 
 class Window:
     def __init__(self):
         self.MainWindow = helper.loadUiWidget("node_tool/gui/view/config.ui")
-        
+        self.tabs = self.MainWindow.findChild(QtGui.QTabWidget, "tabWidget")
+        self.status_bar = self.MainWindow.findChild(QtGui.QStatusBar, "statusbar")
+        self.status_bar.showMessage("Waiting to generate configuration.")
         generate_configuration_push_button = self.MainWindow.findChild(QtGui.QPushButton, "generate_configuration_push_button")
-
+        download_routeros_push_button = self.MainWindow.findChild(QtGui.QPushButton, "download_routeros_push_button")
+        send_to_router_push_button = self.MainWindow.findChild(QtGui.QPushButton, "send_to_router_push_button")
+        
         generate_configuration_push_button.clicked.connect(self.generate_configuration_push_button_pressed)
+        download_routeros_push_button.clicked.connect(self.download_routeros_push_button_pressed)
+        send_to_router_push_button.clicked.connect(self.send_to_router_push_button_pressed)
         
         self.MainWindow.show()
         return
     def generate_configuration_push_button_pressed(self):
-        tabs = self.MainWindow.findChild(QtGui.QTabWidget, "tabWidget")
         review_config_plain_text_edit = self.MainWindow.findChild(QtGui.QPlainTextEdit, "review_config_plain_text_edit")
         operators_callsign_line_edit = self.MainWindow.findChild(QtGui.QLineEdit, "operators_callsign_line_edit")
         operators_location_line_edit = self.MainWindow.findChild(QtGui.QLineEdit, "operators_location_line_edit")
@@ -26,6 +33,40 @@ class Window:
                                        shared_administration_check_box.isChecked(), dhcp_client_on_lan_check_box.isChecked(), route_cache_bug_fix_check_box.isChecked())
         
         review_config_plain_text_edit.setPlainText(str(configuration))
-        tabs.setTabEnabled(1, True)
-        tabs.setCurrentIndex(1)
+        self.tabs.setTabEnabled(1, True)
+        self.tabs.setCurrentIndex(1)
+        self.status_bar.showMessage("Configuration generated; review and revise, then save or send to router.")
         return
+    def download_routeros_push_button_pressed(self):
+        self.status_bar.showMessage("Downloading RouterOS from MikroTik's website.")
+        send_progress_bar = self.MainWindow.findChild(QtGui.QProgressBar, "send_progress_bar")
+        send_progress_bar.setMinimum(0)
+        send_progress_bar.setMaximum(0)
+        self.worker = Worker()
+        self.worker.finished.connect(self.finishedDownloading)
+        self.worker.start()
+        return
+    def finishedDownloading(self):
+        send_progress_bar = self.MainWindow.findChild(QtGui.QProgressBar, "send_progress_bar")
+        send_progress_bar.setMaximum(100)
+        if self.worker.status:
+            send_progress_bar.setValue(33)
+            self.status_bar.showMessage("Done downloading RouterOS; now press update RouterOS.")
+        else:
+            send_progress_bar.setValue(0)
+            self.status_bar.showMessage("Error downloading RouterOS! Manually add firmware to firmware directory, then press update RouterOS.")
+            
+    def send_to_router_push_button_pressed(self):
+        self.tabs.setTabEnabled(2, True)
+        self.tabs.setCurrentIndex(2)
+        self.status_bar.showMessage("Preparing Qto send configuration to router.")
+        return
+
+
+#Inherit from QThread
+class Worker(QtCore.QThread):
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+        
+    def run(self):
+        self.status = helpers.firmware_download()
