5b0ded965cdf2047da8b1e1d84735f1aa96fe910
ryanturner <ryan.e.t@gmail.com>
2014-03-02 22:26:26 -0600

    Tool now downloads the SSH keys as well

--- node_tool/common/helpers.py
+++ node_tool/common/helpers.py
@@ -9,36 +9,42 @@ import zipfile
 import urllib
 
 def firmware_download():
-    directory = "firmware"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    if not os.path.exists("firmware"):
+        os.makedirs("firmware")
     # Download the mikrotik routeros firmware
-    file_name = "all_packages-mipsbe-6.9.zip"
-    url = "http://download2.mikrotik.com/routeros/6.9/"
     try:
-        urllib.urlretrieve(url + file_name, directory + "/" + file_name)
+        urllib.urlretrieve("http://download2.mikrotik.com/routeros/6.9/all_packages-mipsbe-6.9.zip", "firmware/all_packages-mipsbe-6.9.zip")
     except:
         print("Error downloading firmware.")
         return False
     try:
-        with zipfile.ZipFile(directory + "/" + file_name) as zipped_firmware:
-            zipped_firmware.extractall(directory)
+        with zipfile.ZipFile("firmware/all_packages-mipsbe-6.9.zip") as zipped_firmware:
+            zipped_firmware.extractall("firmware")
     except:
         print("Error unzipping firmware.")
         return False
     try:
-        os.remove(directory + "/" + file_name)
+        os.remove("firmware/all_packages-mipsbe-6.9.zip")
     except:
         print("Error deleting firmware zip file.")
         return False
         
     # Get the branding package too
-    file_name = "HamWAN-2.9.48.dpk"
-    url = "https://github.com/HamWAN/hamwan-branding/raw/master/Mikrotik%20Rebranding/"
     try:
-        urllib.urlretrieve(url + file_name, directory + "/" + file_name)
+        urllib.urlretrieve("https://github.com/HamWAN/hamwan-branding/raw/master/Mikrotik%20Rebranding/HamWAN-2.9.48.dpk", "firmware/HamWAN-2.9.48.dpk")
     except:
         print("Error downloading branding package.")
+        return False
+    
+    # Get the SSH keys
+    try:
+        urllib.urlretrieve("https://www.hamwan.org/t/tiki-download_wiki_attachment.php?attId=148&download=y", "firmware/key-dsa-monitoring.txt")
+        urllib.urlretrieve("https://www.hamwan.org/t/tiki-download_wiki_attachment.php?attId=140&download=y", "firmware/key-dsa-osburn.txt")
+        urllib.urlretrieve("https://www.hamwan.org/t/tiki-download_wiki_attachment.php?attId=123&download=y", "firmware/key-dsa-NQ1E.txt")
+        urllib.urlretrieve("https://www.hamwan.org/t/tiki-download_wiki_attachment.php?attId=120&download=y", "firmware/key-dsa-nigel.txt")
+        urllib.urlretrieve("https://www.hamwan.org/t/tiki-download_wiki_attachment.php?attId=119&download=y", "firmware/key-dsa-eo.txt")
+    except:
+        print("Error downloading SSH keys.")
         return False
     return True
         
